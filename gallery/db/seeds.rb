116609e442cebda7e0f1953c9e885b2802d674a1
Alan Hunter <localpunter@sky.com>
2018-08-24 15:20:13 +0100

    artists and exhibits added to seeds.rb

--- gallery/db/seeds.rb
+++ gallery/db/seeds.rb
@@ -1,2 +1,49 @@
 require_relative ('../models/artist.rb')
 require_relative ('../models/exhibit.rb')
+require ('pry-bybug')
+
+Artist.delete_all()
+Exhibit.delete_all()
+
+artist1 = Artist.new({
+  "name" => "artist1"
+  })
+
+artist1.save()
+
+artist2 = Artist.new({
+  "name" => "artist2"
+  })
+
+artist2.save()
+
+exhibit1a = Exhibit.new({
+  "title" => "exhibit1a"
+  "category" => "painting"
+  "artist_id" => artist1.id
+  })
+
+exhibit1a.save()
+
+exhibit1b = Exhibit.new({
+  "title" => "exhibit1b"
+  "category" => "sculpture"
+  "artist_id" => artist1.id
+  })
+
+exhibit1b.save()
+
+exhibit2a = Exhibit.new({
+  "title" => "exhibit2a"
+  "category" => "sculpture"
+  "artist_id" => artist2.id
+  })
+
+exhibit2a.save()
+
+exhibit2b = Exhibit.new({
+  "title" => "exhibit2b"
+  "category" => "painting"
+  })
+
+exhibit2b.save()
