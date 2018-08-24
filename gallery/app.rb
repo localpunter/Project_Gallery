b88c911a044e2f12e448049dfb8038441b387d21
Alan Hunter <localpunter@sky.com>
2018-08-24 16:34:51 +0100

    app.rb updated

--- gallery/app.rb
+++ gallery/app.rb
@@ -2,7 +2,7 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/artists_controller')
 require_relative('controllers/exhibits_controller')
-require_relative('controllers/bitings_controller')
+
 
 get '/' do
   erb( :index )
