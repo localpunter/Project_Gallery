1739774908c22644043e0cb358d14ccf0bacc1d8
Alan Hunter <localpunter@sky.com>
2018-08-27 17:31:09 +0100

    Edit function added, tested and working

--- gallery/models/artist.rb
+++ gallery/models/artist.rb
@@ -24,7 +24,7 @@ class Artist
   end
 
   def update()
-    sql = "UPDATE artists SET (name)
+    sql = "UPDATE artists SET name
     = $1 WHERE id = $2;"
     values = [@name, @id]
     SqlRunner.run(sql, values)
