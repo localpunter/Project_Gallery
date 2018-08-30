a9e2060f8508c6ffbfaa2ec3f9a32fbc94657f74
Alan Hunter <localpunter@sky.com>
2018-08-30 23:40:57 +0100

    grid failed so removed

--- gallery/db/seeds.rb
+++ gallery/db/seeds.rb
@@ -10,16 +10,22 @@ Exhibit.delete_all()
 
 
 artist1 = Artist.new({
-  "name" => "artist1"
+  "name" => "Da Vinci"
   })
 
 artist1.save()
 
 artist2 = Artist.new({
-  "name" => "artist2"
+  "name" => "Dali"
   })
 
 artist2.save()
+
+artist3 = Artist.new({
+  "name" => "Papa Lazarou"
+  })
+
+artist3.save()
 
 category1 = Category.new({
   "genre" => "Painting"
@@ -56,7 +62,7 @@ exhibit1a.save()
 
 exhibit1b = Exhibit.new({
   "title" => "exhibit1b",
-  "category_id" => category2.id,
+  "category_id" => category1.id,
   "artist_id" => artist1.id
   })
 
@@ -64,7 +70,7 @@ exhibit1b.save()
 
 exhibit2a = Exhibit.new({
   "title" => "exhibit2a",
-  "category_id" => category4.id,
+  "category_id" => category1.id,
   "artist_id" => artist2.id
   })
 
@@ -77,6 +83,14 @@ exhibit2b = Exhibit.new({
   })
 
 exhibit2b.save()
+
+exhibit3a = Exhibit.new({
+  "title" => "exhibit3a",
+  "category_id" => category4.id,
+  "artist_id" => artist3.id
+  })
+
+exhibit3a.save()
 
 
 
